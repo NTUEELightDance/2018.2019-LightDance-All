80c5a93f5728a4d8039fc8e2ba6c7c4ba56344b1
Chih-Wei Fang <dodo0822@hs.ntnu.edu.tw>
2018-03-16 15:02:33 +0800

    support local data file

--- control_7688/check.cpp
+++ control_7688/check.cpp
@@ -1,13 +1,31 @@
-#include "PCA9685.h"
+#ifdef USE_INTERNAL_PWM
+    #include "mraa/common.hpp"
+    #include "mraa/pwm.hpp"
+#else
+    #include "PCA9685.h"
+#endif
+
 #include <cstdio>
 
 #include "common.h"
 
-PCA9685 pca(0, 0x40);
+#ifdef USE_INTERNAL_PWM
+    mraa::Pwm pwm_0(18);
+    mraa::Pwm pwm_1(19);
+#else
+    PCA9685 pca(0, 0x40);
+#endif
 
 bool light[16];
 
 int main() {
+    #ifdef USE_INTERNAL_PWM
+        pwm_0.period_ms(2);
+        pwm_1.period_ms(2);
+        pwm_0.enable(true);
+        pwm_1.enable(true);
+    #endif
+
     Config conf;
     conf = read_config();
 
@@ -20,10 +38,15 @@ int main() {
     while(true) {
         int id;
         for(int i = 0; i < 16; ++i) {
-            pca.setPWM(conf.pins[i], 0, (light[i] ? 4095 : 0));
+            #ifdef USE_INTERNAL_PWM
+                if(i == 0) pwm_0.write(light[i] ? 1.0 : 0.0);
+                else if(i == 1) pwm_1.write(light[i] ? 1.0 : 0.0);
+            #else
+                pca.setPWM(conf.pins[i], 0, (light[i] ? 4095 : 0));
+            #endif
         }
         printf("> ");
-        scanf("%d", &id);
+        if(!(~scanf("%d", &id))) break;
         if(id == -1) {
             for(int i = 0; i < 16; ++i) {
                 light[i] = true;
